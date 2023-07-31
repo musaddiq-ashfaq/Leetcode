4911947e58336eea8caef747604664ebbeabd1d2
musaddiq-ashfaq <musasaddiq029@gmail.com>
2023-07-31 15:42:25 +0500

    Added 121

---
+++ 121.Best_Time_To_Buy_And_Sell_Stocks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minP = INT_MAX;
+        int profit = 0;
+        for(int i = 0;i<prices.size();i++)
+        {
+            minP = min(minP,prices[i]);
+            profit = max(profit,prices[i] - minP);
+        }
+        return profit;
+    }
+};
+
+//Time Complexity : O(n)
+//Space Complexity : O(1)
