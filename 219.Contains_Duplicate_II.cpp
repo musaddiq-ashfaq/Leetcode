04ed30e19f020512d7af1d4122c2d441bd7f1aa0
musaddiq-ashfaq <musasaddiq029@gmail.com>
2023-08-06 14:15:27 +0500

    Added 219

---
+++ 219.Contains_Duplicate_II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(int i = 0;i<nums.size();i++)
+        {
+            auto it = mp.find(nums[i]);
+            if(it != mp.end() && abs(it->second - i) <= k)
+            {
+                return true;
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(N)
