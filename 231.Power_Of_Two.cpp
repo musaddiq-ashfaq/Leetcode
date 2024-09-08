8cf26c6f45c2e8ab316b45898efe077b8f0d4277
musaddiq-ashfaq <musasaddiq029@gmail.com>
2024-09-08 14:34:47 +0500

    Added 231

---
+++ 231.Power_Of_Two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int i = 0;
+        while(true){
+            if(pow(2,i) == n)
+                return true;
+            else if(pow(2,i) > n)
+                return false;
+            else
+                i++;
+        }
+        return false;
+    }
+};
