4f3c36ed631b8fa2308c694bac72e052a05f0617
musaddiq-ashfaq <musasaddiq029@gmail.com>
2024-09-29 23:57:12 +0500

    Added 859

--- 859.Buddy_Strings.cpp
+++ 859.Buddy_Strings.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool buddyStrings(string A, string B) {
-        if (A.size() != B.size())
+    bool buddyStrings(string s, string goal) {
+        if (s.size() != goal.size())
             return false;
-        if (A == B) {
+        if (s == goal) {
             vector<int> freq(26, 0);
-            for (char c : A) {
+            for (char c : s) {
                 freq[c - 'a']++;
                 if (freq[c - 'a'] > 1)
                     return true;
             }
             return false;
         }
+
         vector<int> diff;
-        for (int i = 0; i < A.size(); i++) {
-            if (A[i] != B[i])
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != goal[i])
                 diff.push_back(i);
         }
 
-        if (diff.size() == 2 && A[diff[0]] == B[diff[1]] &&
-            A[diff[1]] == B[diff[0]])
+        if (diff.size() == 2 && s[diff[0]] == goal[diff[1]] &&
+            s[diff[1]] == goal[diff[0]])
             return true;
-
         return false;
     }
 };
