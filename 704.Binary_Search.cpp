011dd7595801fad8861b71aec8a9bc123997fae0
musaddiq-ashfaq <musasaddiq029@gmail.com>
2025-01-13 12:23:16 +0500

    Added 704

---
+++ 704.Binary_Search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while(l<=r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                r--;
+            else 
+                l++;
+        }
+        return -1;
+    }
+};
