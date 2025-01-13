45cfcd89d3f072559cc1fcc1a6740865afeeb902
musaddiq-ashfaq <musasaddiq029@gmail.com>
2025-01-13 23:46:36 +0500

    Added 739

---
+++ 739.Daily_Temperatures.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>> st;
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        st.push({temperatures[n - 1], n-1});
+
+        for (int i = n - 2; i >= 0; i--) {
+            while(!st.empty() && st.top().first <= temperatures[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                st.push({temperatures[i],i});
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = st.top().second-i;
+                st.push({temperatures[i],i});
+            }
+        }
+        return ans;
+    }
+};
